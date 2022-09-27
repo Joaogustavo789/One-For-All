1653f2502a9d44d0b52e4db0fa1f636af1856a25
Joao Gustavo <jgustavomendonca@gmail.com>
2022-09-26 23:05:13 -0300

    Requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(hr.historico_de_reproducoes) AS quantidade_musicas_no_historico
+FROM SpotifyClone.historico_de_reproducoes AS hr
+INNER JOIN SpotifyClone.usuario AS us
+ON hr.usuario_id = us.usuario_id
+GROUP BY us.usuario
+HAVING us.usuario = 'Barbara Liskov'
